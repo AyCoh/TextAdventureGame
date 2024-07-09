97eeebdf013edab3f5e869834407acf2dd130a1b
aycoh <aycohdev@gmail.com>
2024-07-08 19:37:24 -0700

    Migrated from Old Account

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.css'; // basic bootstrap styles
+import './css/App.css'; // custom general styles - keep at the end to override bootstrap
+import './css/media.css'; // custom media queries for responsive design
+import './css/fa-all.css'; // fa icons
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
